19a17635d086643d1012eb640097d242258cc854
Caveman Bian <51192378+bianzheng123@users.noreply.github.com>
2019-09-04 15:47:37 +0800

    add the create room function

---
+++ MyGoMoKu/Assets/Scripts/MatckMaker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class MatckMaker : MonoBehaviour {
+    NetworkManager manager;
+    public string roomName;
+    private void Start()
+    {
+        manager = NetworkManager.singleton;
+        if(manager.matchMaker == null)
+        {
+            manager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoomName(string name)
+    {
+        roomName = name;
+    }
+
+    public void OnCreateNameBtn()
+    {
+        manager.matchMaker.CreateMatch(roomName,3,true,"","","",0,0,manager.OnMatchCreate);
+    }
+}
