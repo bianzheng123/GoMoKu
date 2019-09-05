7f64a27cc20ecd8f275f5a752e70888999227311
Caveman Bian <51192378+bianzheng123@users.noreply.github.com>
2019-09-05 13:40:54 +0800

    add the function of refresh

--- MyGoMoKu/Assets/Scripts/MatckMaker.cs
+++ MyGoMoKu/Assets/Scripts/MatckMaker.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
 public class MatckMaker : MonoBehaviour {
     NetworkManager manager;
     public string roomName;
+    List<GameObject> roomList = new List<GameObject>();
+    [SerializeField]
+    GameObject btn;
+    [SerializeField]
+    Transform parent;
     private void Start()
     {
         manager = NetworkManager.singleton;
@@ -22,5 +28,34 @@ public class MatckMaker : MonoBehaviour {
     public void OnCreateNameBtn()
     {
         manager.matchMaker.CreateMatch(roomName,3,true,"","","",0,0,manager.OnMatchCreate);
+    }
+
+    public void OnRefreshBtn()
+    {
+        manager.matchMaker.ListMatches(0,10,"",true,0,0,OnMatchList);
+    }
+
+    public void OnMatchList(bool success,string extendedInfo,List<MatchInfoSnapshot> matches)
+    {
+        if (!success)
+        {
+            Debug.Log("error");
+            return;
+        }
+        ClearList();
+        foreach (MatchInfoSnapshot match in matches)
+        {
+            GameObject go = Instantiate(btn,parent);
+            roomList.Add(go);
+        }
+    }
+
+    void ClearList()
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Destroy(roomList[i]);
+        }
+        roomList.Clear();
     }
 }
